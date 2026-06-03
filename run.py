274ab99f9cf45c8a0f62f1efe264e8f493342b61
波 王 <wangbo@tingyun.com>
2026-06-03 11:28:39 +0800

    add config

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ load_dotenv(Path(__file__).parent / ".env")
 
 
 def main():
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8002, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8002, reload=False)
 
 
 if __name__ == "__main__":
